041eae9ae517143982f939a6ea2862d37fc16f53
Yevhenii Kolosenko <kolos181@gmail.com>
2018-08-18 13:42:50 +0300

    created components for buttons in navbar. Idea is to switch them with help of named router outlet, instead of using jQuery

---
+++ src/app/components/navbar-buttons/edit-and-del-buttons/edit-and-del-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-and-del-buttons',
+  templateUrl: './edit-and-del-buttons.component.html',
+  styleUrls: ['./edit-and-del-buttons.component.css']
+})
+export class EditAndDelButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
